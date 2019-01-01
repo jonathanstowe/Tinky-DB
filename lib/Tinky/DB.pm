d1a115a17aaad4854783fdba98782e0fd57c2cee
Jonathan Stowe <jns@gellyfish.co.uk>
2019-01-01 20:43:05 +0000

    Basic working version

--- lib/Tinky/DB.pm
+++ lib/Tinky/DB.pm
@@ -5,31 +5,31 @@ use Red;
 
 module Tinky::DB {
 
-    # use Tinky;
-
-    model Workflow is Tinky::Workflow  {
-        has Int $.id            is serial;
-        has Str $.name          is column(:unique);
-        has     @.states        is relationship({ .workflow-id }, model => 'State' );
-        has     @.transitions   is relationship({ .workflow-id }, model => 'Transition' );
+    model Workflow is Tinky::Workflow is table('tinky_workflow') is rw {
+        has Int $.id                is serial;
+        has Str $.name              is column(:unique);
+        has Int $.initial-state-id  is referencing(column => 'id', model => 'Tinky::DB::State', require => 'Tinky::DB');
+        has     $.initial-state     is relationship({ .initial-state-id }, model => 'Tinky::DB::State', require => 'Tinky::DB');
+        has     @.states            is relationship({ .workflow-id }, model => 'Tinky::DB::State', require => 'Tinky::DB' );
+        has     @.transitions       is relationship({ .workflow-id }, model => 'Tinky::DB::Transition', require => 'Tinky::DB' );
     }
 
-    model State is Tinky::State {
+    model State is Tinky::State is table('tinky_state') is rw {
         has Int $.id            is serial;
         has Str $.name          is column;
-        has Int $.workflow-id   is referencing(model => 'Workflow', column => 'id');
-        has     $.workflow      is relationship({ .workflow-id }, model => 'Workflow');
+        has Int $.workflow-id   is referencing(model => 'Tinky::DB::Workflow', column => 'id', require => 'Tinky::DB');
+        has     $.workflow      is relationship({ .workflow-id }, model => 'Tinky::DB::Workflow', require => 'Tinky::DB');
     }
 
-    model Transition is Tinky::Transition {
+    model Transition is Tinky::Transition is table('tinky_transition') is rw {
         has Int $.id            is serial;
         has Str $.name          is column;
-        has Int $.workflow-id   is referencing(model => 'Workflow', column => 'id');
-        has     $.workflow      is relationship({ .workflow-id }, model => 'Workflow');
-        has Int $.from-id       is referencing(model => 'State', column => 'id');
-        has     $.from          is relationship({  .from-id }, model => 'State' );
-        has Int $.to-id         is referencing(model => 'State', column => 'id');
-        has     $.to            is relationship({  .to-id }, model => 'State' );
+        has Int $.workflow-id   is referencing(model => 'Tinky::DB::Workflow', column => 'id', require => 'Tinky::DB');
+        has     $.workflow      is relationship({ .workflow-id }, model => 'Tinky::DB::Workflow', require => 'Tinky::DB');
+        has Int $.from-id       is referencing(model => 'Tinky::DB::State', column => 'id', require => 'Tinky::DB');
+        has     $.from          is relationship({  .from-id }, model => 'Tinky::DB::State', require => 'Tinky::DB' );
+        has Int $.to-id         is referencing(model => 'Tinky::DB::State', column => 'id', require => 'Tinky::DB');
+        has     $.to            is relationship({  .to-id }, model => 'Tinky::DB::State', require => 'Tinky::DB' );
 
     }
 }
